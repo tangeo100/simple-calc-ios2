6b2691ada46abde9a384da7094a2fbadf6377119
Ronald Tang <rnld.tang@gmail.com>
2018-10-15 21:49:01 -0700

    Initial Commit

---
+++ simple-calc/simple-calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simple-calc
+//
+//  Created by Ronald Tang on 10/15/18.
+//  Copyright © 2018 Ronald Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
